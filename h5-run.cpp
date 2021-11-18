98af1b05cf32e695c2fe790197d2b66713cb16b8
tomeichlersmith <eichl008@umn.edu>
2021-11-18 13:26:43 -0600

    attempt to append not working with H5Easy::dump

--- h5-run.cpp
+++ h5-run.cpp
@@ -10,8 +10,7 @@
 // Compound datatype test :: May 2021
 // //////////////////////////////////
 
-#include <highfive/H5File.hpp>
-
+#include <highfive/H5Easy.hpp>
 
 typedef struct {
     double width;
@@ -29,21 +28,13 @@ HIGHFIVE_REGISTER_TYPE(Size2D, create_compound_Size2D)
 int main() {
     const std::string FILE_NAME("compounds_test.h5");
 
-    HighFive::File file(FILE_NAME, HighFive::File::Truncate);
-
-    HighFive::DataSpace space({1,1},{1,HighFive::DataSpace::UNLIMITED});
-    HighFive::DataSetCreateProps props;
-    props.add(HighFive::Chunking(std::vector<hsize_t>{1,1}));
+    H5Easy::File file(FILE_NAME, HighFive::File::Truncate);
 
     auto t1 = create_compound_Size2D();
     t1.commit(file, "Size2D");
 
-    auto dataset = file.createDataSet(
-        "entries", space, create_compound_Size2D(), props);
-
     std::vector<Size2D> dims = {{1., 2.5}, {3., 4.5}};
-    for (auto const& d : dims) {
-      std::vector<Size2D> entry{1,d};
-      dataset.write(entry);
+    for (std::size_t i_entry{0}; i_entry < dims.size(); i_entry++) {
+      H5Easy::dump(file, "entries", dims.at(i_entry), {i_entry});
     }
 }
