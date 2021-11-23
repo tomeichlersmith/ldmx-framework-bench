9494d81e6e4d4a50081a2aa664d0b6155b9ba4ec
tomeichlersmith <eichl008@umn.edu>
2021-11-23 17:42:02 -0600

    split struct in boost.serialization style

--- h5-run.cpp
+++ h5-run.cpp
@@ -11,30 +11,51 @@
 // //////////////////////////////////
 
 #include <highfive/H5Easy.hpp>
+#include <type_traits>
 
 typedef struct {
-    double width;
-    double height;
+  double width;
+  double height;
+  std::unordered_map<std::string, double> split() const {
+    return {{"width", width}, {"height", height}};
+  }
 } Size2D;
 
+class MyFile {
+  H5Easy::File h5_file_;
+  long unsigned int i_entry_;
+ public:
+  MyFile(const std::string& name = "test.h5")
+      : h5_file_{name, HighFive::File::Truncate}, i_entry_{0} {}
 
-HighFive::CompoundType create_compound_Size2D() {
-    return {{ "width",  HighFive::AtomicType<double>{} },
-            { "height", HighFive::AtomicType<double>{} }};
-}
+  void next() { i_entry_++; }
 
-HIGHFIVE_REGISTER_TYPE(Size2D, create_compound_Size2D)
+  template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
+  void push(std::string const& name, T const& v) {
+    H5Easy::dump(h5_file_, name, v, {i_entry_});
+  }
+
+  template <typename T, std::enable_if_t<std::is_class_v<T>, bool> = true>
+  void push(std::string const& name, T const& o) {
+    for (auto const& [key, val] : o.split()) {
+      push(name + "/" + key, val);
+    }
+  }
+};
 
 int main() {
-    const std::string FILE_NAME("compounds_test.h5");
-
-    H5Easy::File file(FILE_NAME, HighFive::File::Truncate);
-
-    auto t1 = create_compound_Size2D();
-    t1.commit(file, "Size2D");
-
+  {
+    MyFile file;
     std::vector<Size2D> dims = {{1., 2.5}, {3., 4.5}};
     for (std::size_t i_entry{0}; i_entry < dims.size(); i_entry++) {
-      H5Easy::dump(file, "entries", dims.at(i_entry), {i_entry});
+      file.push("dims", dims.at(i_entry));
+
+      /*
+      std::vector<int> entries(i_entry, i_entry);
+      file.push("entries", entries);
+      */
+
+      file.next();
     }
+  }
 }
