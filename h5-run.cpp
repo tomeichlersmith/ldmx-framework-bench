775dc0b56a8914b4c36a762337815550a843472d
tomeichlersmith <eichl008@umn.edu>
2021-11-24 15:40:00 -0600

    able to write nested vector of arithmetic types
    ugly and hard to generalize, need to use fancier struct methods in order
    to generalize to other containers and such

--- h5-run.cpp
+++ h5-run.cpp
@@ -13,10 +13,28 @@
 #include <highfive/H5Easy.hpp>
 #include <type_traits>
 
+/*
+ * Type deduction and specialization structs for load/save
+template <typename T>
+struct loader {
+  static T load(group,std::string const& name) {
+    T ret;
+    ret.load(subgroup,name);
+    return ret;
+  }
+}
+
+template <typename T>
+using load = loader<T>::load
+*/
+
 class MyFile {
   H5Easy::File h5_file_;
-  long int i_entry_;
-  long int entries_;
+  bool write_;
+  long unsigned int i_entry_;
+  long unsigned int entries_;
+  std::unordered_map<std::string,long unsigned int>
+    sub_entries_;
  public:
   class MyGroup {
     MyFile& file_;
@@ -35,37 +53,32 @@ class MyFile {
   };
  public:
   MyFile(const std::string& name = "test.h5", bool write = false)
-      : h5_file_{name, write ? HighFive::File::Truncate : HighFive::File::ReadOnly} {
-    if (write) {
-      i_entry_ = 0; 
-      entries_ = 0; // not started yet
-      // using 0 instead of -1 here means that file.next()
-      // should be called at the end of the loop while writing
-    } else {
+      : h5_file_{name, write ? HighFive::File::Truncate : HighFive::File::ReadOnly}, write_{write}, i_entry_{0}, entries_{0} {
+    if (not write) {
       // need to grab number of entries from specific data set
       entries_ = H5Easy::getSize(h5_file_,"i_entry");
-      i_entry_ = -1; // not started yet
-      // goes up to valid index upon entry into 'while' loop
     }
   }
 
   bool next() { 
-    if (write) {
-      i_entry_++; 
+    i_entry_++;
+    if (write_) {
       entries_++;
+      return true;
     } else {
-      i_entry_++;
       return i_entry_ < entries_;
     }
   }
 
   template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
   void save(std::string const& name, T const& v) {
+    std::cout << name << " saving " << i_entry_ << std::endl;
     H5Easy::dump(h5_file_, name, v, {i_entry_});
   }
 
   template <typename T, std::enable_if_t<std::is_arithmetic_v<T>, bool> = true>
   T load(std::string const& name) const {
+    std::cout << name << " loading " << i_entry_ << std::endl;
     return H5Easy::load<T>(h5_file_, name, {i_entry_});
   }
 
@@ -79,6 +92,20 @@ class MyFile {
     T v;
     v.load(MyGroup(*this,name));
     return v;
+  }
+
+  template <typename T, std::enable_if_t<std::is_arithmetic_v<T>,bool> = true>
+  void save_vector(std::string const& name, std::vector<T> const& vec) {
+    auto sub_entry_it{sub_entries_.find(name)};
+    if (sub_entry_it == sub_entries_.end()) {
+      sub_entries_[name] = i_entry_;
+    }
+    auto begin = sub_entries_.at(name);
+    H5Easy::dump(h5_file_, name+"/begin", begin, {i_entry_});
+    H5Easy::dump(h5_file_, name+"/end", begin+vec.size(), {i_entry_});
+    for (std::size_t i_vec{0}; i_vec < vec.size(); i_vec++)
+      H5Easy::dump(h5_file_, name+"/values", vec.at(i_vec), {begin+i_vec});
+    sub_entries_[name] += vec.size();
   }
 };
 
@@ -104,6 +131,7 @@ int main() {
   std::vector<Size2D> dims = {{1., 2.5}, {3., 4.5}};
   std::vector<double> areas = { 1.*2.5 , 3.*4.5 };
   std::vector<int> i_entries  = { 1 , 2 };
+  std::vector<std::vector<int>> entry_entries = {{1}, {2,2}};
 
   std::cout << "Writing..." << std::endl;
   {
@@ -112,6 +140,7 @@ int main() {
       file.save("dims", dims.at(i_entry));
       file.save("areas", areas.at(i_entry));
       file.save("i_entry", i_entries.at(i_entry));
+      file.save_vector("nested_entries", entry_entries.at(i_entry));
 
       file.next();
     }
@@ -121,7 +150,7 @@ int main() {
   {
     MyFile file("test.h5");
     std::size_t i_entry_actual{0};
-    while (file.next()) {
+    do {
       auto d = file.load<Size2D>("dims");
       if (d != dims.at(i_entry_actual)) {
         std::cout << "error reading dims" << std::endl;
@@ -138,6 +167,6 @@ int main() {
       }
 
       i_entry_actual++;
-    }
+    } while (file.next());
   }
 }
