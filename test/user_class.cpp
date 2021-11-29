9f170dbe4c440b601c3982b0f500beef21c559ed
tomeichlersmith <eichl008@umn.edu>
2021-11-29 15:18:47 -0600

    nested user class functional with current design

--- test/user_class.cpp
+++ test/user_class.cpp
@@ -5,19 +5,41 @@
 #include "fire/h5/File.hpp"
 #include "fire/h5/DataSet.hpp"
 
+// test a plain old data user class
 class Size2D {
   double width;
   double height;
  public:
   Size2D() = default; // empty, default constructor necessary for serialization
   Size2D(double w, double h) : width(w), height(h) {}
+  // for testing
+  bool operator==(Size2D const& other) {
+    return width == other.width and height == other.height;
+  }
+ private:
+  friend class fire::h5::DataSet<Size2D>;
   void attach(fire::h5::DataSet<Size2D> &set) {
     set.attach("width",width);
     set.attach("height",height);
   }
+};
+
+// test a user class that is a member of a user class
+class Size3D {
+  double depth;
+  Size2D xsec;
+ public:
+  Size3D() = default; // required for serialization
+  Size3D(double w, double h, double d) : xsec(w,h), depth(d) {}
   // for testing
-  bool operator==(Size2D const& other) {
-    return width == other.width and height == other.height;
+  bool operator==(Size3D const& other) {
+    return xsec == other.xsec and depth == other.depth;
+  }
+ private:
+  friend class fire::h5::DataSet<Size3D>;
+  void attach(fire::h5::DataSet<Size3D> &set) {
+    set.attach("depth",depth);
+    set.attach("xsec",xsec);
   }
 };
 
@@ -28,10 +50,16 @@ BOOST_AUTO_TEST_CASE(user_class) {
   dims.emplace_back(3.,4.5);
   dims.emplace_back(-9.8,-420.);
 
+  std::vector<Size3D> nested;
+  nested.emplace_back(3.0,2.,1);
+  nested.emplace_back(-3.0,20.,0.1);
+  nested.emplace_back(98.,32.,-11);
+
   try { // Writing
     fire::h5::File file(filename,true);
     for (std::size_t i_entry{0}; i_entry < dims.size(); i_entry++) {
       file.add("dims", dims.at(i_entry));
+      file.add("nested", nested.at(i_entry));
 
       // needed for event counting, standing in for event header
       file.add("i_entry", i_entry);
@@ -48,6 +76,9 @@ BOOST_AUTO_TEST_CASE(user_class) {
     do {
       auto d = file.get<Size2D>("dims");
       BOOST_CHECK(d == dims.at(i_entry));
+
+      auto s = file.get<Size3D>("nested");
+      BOOST_CHECK(s == nested.at(i_entry));
 
       i_entry++;
     } while (file.next());
