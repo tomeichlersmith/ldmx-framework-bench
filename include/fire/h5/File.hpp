12584a8ac9019d17ed4048f9a78e834f1333e522
tomeichlersmith <eichl008@umn.edu>
2021-11-30 08:04:42 -0600

    documentation for Event class

--- include/fire/h5/File.hpp
+++ include/fire/h5/File.hpp
@@ -11,12 +11,40 @@ namespace h5 {
 /// forward declaration for friendship
 class File;
 
-/// all aboard!
+/**
+ * Event class for interfacing with processors
+ *
+ * This class is what is given to the processors to 'get' data from
+ * and/or 'add' data to.
+ *
+ * The 'File' class is a friend class so that it can register
+ * itself with the Event if it is an input file and it can iterate
+ * the current entry index by calling 'Event::next'.
+ */
 class Event {
  public:
+  /**
+   * Default constructor
+   *
+   * Besides the name of the pass, the input_file handle is
+   * initialized to a nullptr to signify that no input file has
+   * been "registered" yet and i_entry_ is started at zero.
+   *
+   * @param[in] pass name of current processing pass
+   */
   Event(const std::string& pass) : pass_{pass}, input_file_{nullptr}, i_entry_{0} {}
-  ~Event() = default;
 
+  /**
+   * add a piece of data to the event
+   *
+   * @TODO check if two add's of the same name are done per event
+   * @TODO add in handling of the pass name
+   * @throw Exception if input DataType doesn't match the type stored in the data set
+   *
+   * @tparam[in] DataType type of data being added
+   * @param[in] name name of data being added
+   * @param[in] data actual value of data being added
+   */
   template <typename DataType>
   void add(std::string const& name, DataType& data) {
     auto set_it{sets_.find(name)};
@@ -33,6 +61,18 @@ class Event {
     }
   }
 
+  /**
+   * get a piece of data from the event
+   *
+   * @TODO add in handling of the pass name
+   *
+   * @throw Exception if requested data doesn't exist
+   * @throw Exception if requested DataType doesn't match type in data set
+   *
+   * @tparam[in] DataType type of requested data
+   * @param[in] name Name of requested data
+   * @return const reference to data in event
+   */
   template <typename DataType>
   DataType const& get(std::string const& name) {
     auto set_it{sets_.find(name)};
@@ -58,24 +98,62 @@ class Event {
     }
   }
  private:
+  /**
+   * The File is the Event's friend,
+   * this allows the File to handle the core iteration procedure
+   * while preventing other classes from accessing these methods.
+   */
   friend class File;
+
+  /**
+   * Go through and save the current in-memory objects into 
+   * the output file at the input index.
+   *
+   * @param[in] f output HDF5 file to write to
+   * @param[in] i index of dataset to write to
+   */
   void save(H5Easy::File& f, unsigned long int i) {
     for (auto& [_, set] : sets_) set->save(f, i);
   }
+
+  /**
+   * Go through and load the input index into the in-memory
+   * objects from the input file.
+   *
+   * @param[in] f input HDF5 file to read from
+   * @param[in] i index of dataset to read from
+   */
   void load(H5Easy::File& f, unsigned long int i) {
     for (auto& [_, set] : sets_) set->load(f, i);
   }
+
+  /**
+   * Attach a HDF5 file to this event as the input file
+   *
+   * @note We store the input file as a pointer, but
+   * don't clean-up later.
+   *
+   * @param[in] f reference to HDF5 input file
+   */
   void setInputFile(H5Easy::File& f) {
     input_file_ = &f;
   }
+
+  /**
+   * Move to the next event
+   */
   void next() {
     i_entry_++;
   }
 
  private:
+  /// pointer to input file (maybe nullptr)
   H5Easy::File* input_file_;
+  /// name of current processing pass
   std::string pass_;
+  /// list of datasets being processed
   std::unordered_map<std::string, std::unique_ptr<BaseDataSet>> sets_;
+  /// current index in the datasets
   long unsigned int i_entry_;
 };
 
