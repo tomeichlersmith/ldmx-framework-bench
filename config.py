4d1be73d9e2264bec1dcacad4dfe884191de0220
tomeichlersmith <eichl008@umn.edu>
2021-12-28 09:30:24 -0600

    add benchmarking github action

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ p = fire.cfg.Process('bench')
 import sys
 p.event_limit = int(sys.argv[1])
 
-p.output_file = fire.cfg.OutputFile(f'output_bench_{p.event_limit}.h5')
+import os
+os.makedirs('output')
+p.output_file = fire.cfg.OutputFile(f'output/output_bench_{p.event_limit}.h5')
 
 p.sequence = [ fire.cfg.Producer('make','bench::Produce','Bench') ]
