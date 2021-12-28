fd8c644d8769218ebe29e247721c20adfaae0059
tomeichlersmith <eichl008@umn.edu>
2021-12-28 09:40:14 -0600

    update producer with random data, recursive submodule checkout

--- Bench/src/Bench/Produce.cxx
+++ Bench/src/Bench/Produce.cxx
@@ -2,16 +2,27 @@
 #include "Framework/EventProcessor.h"
 #include "Bench/Event/Hit.h"
 
+#include <random>
+
 namespace bench {
 
 class Produce : public framework::Producer {
+  std::mt19937 rng;
+  std::uniform_int_distribution<std::size_t> rand_size;
+  std::uniform_real_distribution<double> rand_value;
  public:
   Produce(const std::string& name, framework::Process& p)
-    : framework::Producer(name,p) {}
+    : framework::Producer(name,p),
+    rng{},
+    rand_size{1,100},
+    rand_value{0.,1.}
+  {}
   ~Produce() = default;
   void produce(framework::Event& event) final override {
-    Hit h;
-    event.add("hit",h);
+    std::vector<double> rand_data;
+    rand_data.resize(rand_size(rng));
+    for (double& val : rand_data) val = rand_value(rng);
+    event.add("randdata", rand_data);
   }
 };  // Produce
 
