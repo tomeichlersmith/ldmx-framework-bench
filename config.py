4ec0cf24646c4cb7bfc6acad25515a1d7d2196e1
tomeichlersmith <eichl008@umn.edu>
2021-12-29 09:32:50 -0600

    put num events in output file name

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import sys
 p.maxEvents = int(sys.argv[1])
 
 import os
-os.makedirs('output')
-p.outputFiles = ['output/output.root']
+os.makedirs('output',exist_ok=True)
+p.outputFiles = [f'output/output_{p.maxEvents}.root']
 
 p.sequence = [ ldmxcfg.Producer('make','bench::Produce','Bench') ]
