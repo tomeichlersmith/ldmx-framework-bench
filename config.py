17a3645144e121ef2e8e4bc0bad655db86b68679
tomeichlersmith <eichl008@umn.edu>
2022-01-03 10:00:18 -0600

    bump to fire version with manual write buffers

--- config.py
+++ config.py
@@ -7,7 +7,12 @@ import sys
 p.event_limit = int(sys.argv[1])
 
 import os
-os.makedirs('output')
-p.output_file = fire.cfg.OutputFile(f'output/output_bench_{p.event_limit}.h5')
+os.makedirs('output',exist_ok=True)
+p.output_file = fire.cfg.OutputFile(
+        f'output/output_bench_{p.event_limit}.h5',
+        rows_per_chunk = 10000,
+        compression_level = 6,
+        shuffle = False
+        )
 
 p.sequence = [ fire.cfg.Producer('make','bench::Produce','Bench') ]
