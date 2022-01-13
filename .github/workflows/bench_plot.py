746a883349bded17ed2ba5880bdc80dccb773d98
tomeichlersmith <eichl008@umn.edu>
2022-01-13 15:23:57 -0600

    update bench plotting script to do both modes

--- .github/workflows/bench_plot.py
+++ .github/workflows/bench_plot.py
@@ -8,7 +8,7 @@ of the same name and then copied here for automation.
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def bench_plot(events, hdf5_time, hdf5_size, root_time, root_size) :
+def bench_plot(events, hdf5_time, hdf5_size, root_time, root_size, run_mode) :
     """
     Parameters
     ----------
@@ -17,7 +17,7 @@ def bench_plot(events, hdf5_time, hdf5_size, root_time, root_size) :
     """
     fig, ((raw_time,raw_size),(ratio_time,ratio_size)) = plt.subplots(ncols=2,nrows=2, sharex='col')
     fig.set_size_inches(11,7)
-    plt.suptitle('Comparison Between HDF5 and ROOT')
+    plt.suptitle(f'Comparison Between HDF5 and ROOT : {run_mode} Mode')
     plt.subplots_adjust(wspace=0.3, hspace=0.)
 
     raw_time.set_ylabel('Real Time [s]')
@@ -43,17 +43,6 @@ def bench_plot(events, hdf5_time, hdf5_size, root_time, root_size) :
     ratio_size.plot(events, hdf5_size/root_size,
                color='black')
 
-def plot_csv(name) :
-    data = pd.read_csv(name)
-    
-    bench_plot(
-        data[data['serializer']=='hdf5']['events'].to_numpy(),
-        data[data['serializer']=='hdf5']['time'].to_numpy(),
-        data[data['serializer']=='hdf5']['size'].to_numpy(),
-        data[data['serializer']=='root']['time'].to_numpy(),
-        data[data['serializer']=='root']['size'].to_numpy()
-    )
-
 def main() :
     import sys, os
 
@@ -66,8 +55,29 @@ def main() :
             sys.stderr.write(f'ERROR: {data_file} not accessible.\n')
             sys.exit(2)
 
-        plot_csv(data_file)
-        plt.savefig(data_file.replace('csv','pdf'))
+        data = pd.read_csv(name)
+    
+        prod = data[data['mode']=='produce']
+    
+        bench_plot(
+            prod[prod['serializer']=='hdf5']['events'].to_numpy(),
+            prod[prod['serializer']=='hdf5']['time'].to_numpy(),
+            prod[prod['serializer']=='hdf5']['size'].to_numpy(),
+            prod[prod['serializer']=='root']['time'].to_numpy(),
+            prod[prod['serializer']=='root']['size'].to_numpy(),
+            'Production')
+        plt.savefig('production_'+data_file.replace('csv','pdf'))
+        plt.clf()
+
+        reco = data[data['mode']=='recon']
+        bench_plot(
+            reco[reco['serializer']=='hdf5']['events'].to_numpy(),
+            reco[reco['serializer']=='hdf5']['time'].to_numpy(),
+            reco[reco['serializer']=='hdf5']['size'].to_numpy(),
+            reco[reco['serializer']=='root']['time'].to_numpy(),
+            reco[reco['serializer']=='root']['size'].to_numpy(),
+            'Reconstruction')
+        plt.savefig('recon_'+data_file.replace('csv','pdf'))
         plt.clf()
 
     sys.exit(0)
